4a3357c3074837ed10f55bbee6a258e6f9d54ed1
your name <long.cv.61cntt@ntu.edu.vn>
2022-04-08 07:46:12 +0700

    add

--- BaiKT2/app/src/main/java/chauvinhlong/baikt2/MainActivity.java
+++ BaiKT2/app/src/main/java/chauvinhlong/baikt2/MainActivity.java
@@ -5,22 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rGR;
     Button btnTip;
-    RadioButton r13,r15,r18;
+    RadioButton radioButton;
+    EditText edtNhap,Tinh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rGR = findViewById(R.id.rGR);
         btnTip = findViewById(R.id.btnTip);
-        r13 = findViewById(R.id.r13);
-        r15 = findViewById(R.id.r15);
-        r18 = findViewById(R.id.r18);
+        edtNhap = findViewById(R.id.edtNhap);
+        Tinh = findViewById(R.id.Tinh);
 
-        
+        btnTip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int radioID =  rGR.getCheckedRadioButtonId();
+                radioButton = findViewById(radioID);
+
+
+            }
+        });
     }
+    public void checkRBT(View view) {
+        int radioID =  rGR.getCheckedRadioButtonId();
+        radioButton = findViewById(radioID);
+        Toast.makeText(this,"Selected Radio Button: "+radioButton.getText(),Toast.LENGTH_SHORT).show();
+        }
 
     public void Tip(View view) {
     }
