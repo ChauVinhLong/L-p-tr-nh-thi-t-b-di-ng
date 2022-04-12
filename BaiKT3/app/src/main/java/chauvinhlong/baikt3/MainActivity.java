79bc36ea8d164b13f2ab0d38ad8b7807b544895e
your name <long.cv.61cntt@ntu.edu.vn>
2022-04-12 13:34:25 +0700

    add

--- BaiKT3/app/src/main/java/chauvinhlong/baikt3/MainActivity.java
+++ BaiKT3/app/src/main/java/chauvinhlong/baikt3/MainActivity.java
@@ -2,13 +2,33 @@ package chauvinhlong.baikt3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] listData;
+    private ArrayAdapter<String> adapter;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        listData = context.getResources().getStringArray(R.array.ListViewActivity);
+
+        ListView lv=(ListView) findViewById(R.id.lv);
+
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listData);
+
+        lv.setAdapter(adapter);
     }
 }
